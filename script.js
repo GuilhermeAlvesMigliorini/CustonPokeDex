af21d452e3f48c3f7eb5d5e823a901fbd8c502cc
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-01-22 13:27:49 -0300

    functions refactored

--- script.js
+++ script.js
@@ -2,24 +2,31 @@ const pokeName = document .createElement('h2')
 const pokeImage = document.createElement('img')
 const exhibitionArea = document.querySelector('.exhibitionArea')
 let number = 1
+
 const pokeURL = `https://pokeapi.co/api/v2/pokemon/${number}`
-console.log(number)
 
 const pokemon = fetch(pokeURL)
 .then((resp) => resp.json())
 
 
-const grtData = async () => {
+const getData = async () => {
   pokemon.then((a) => {
-    // console.log(a.name)
-    pokeName.innerText = a.name
-    exhibitionArea.appendChild(pokeName)
-    pokeImage.src = a.sprites.front_default
-    exhibitionArea.appendChild(pokeImage)
+    pokemonName(a)
+    pokemonSprite(a)
   })
   
 }
 
- grtData()
+ getData()
 
+
+ function pokemonName(p){
+  pokeName.innerText = p.name
+  exhibitionArea.appendChild(pokeName)
+ }
+
+ function pokemonSprite(p){
+  pokeImage.src = p.sprites.front_default
+  exhibitionArea.appendChild(pokeImage)
+ }
  
