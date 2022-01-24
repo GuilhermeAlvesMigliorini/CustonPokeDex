7094ea788c1922de9139c777fa78b565fae148e4
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-01-24 11:32:03 -0300

    search pokemon functionality added

--- script.js
+++ script.js
@@ -1,23 +1,11 @@
 const pokeName = document .createElement('h2')
 const pokeImage = document.createElement('img')
 const exhibitionArea = document.querySelector('.exhibitionArea')
-let number = 1
-
-const pokeURL = `https://pokeapi.co/api/v2/pokemon/${number}`
-
-const pokemon = fetch(pokeURL)
-.then((resp) => resp.json())
+let pokeIndentifier = 1
 
 
-const getData = async () => {
-  pokemon.then((a) => {
-    pokemonName(a)
-    pokemonSprite(a)
-  })
-  
-}
 
- getData()
+pokeFetch()
 
 
  function pokemonName(p){
@@ -30,3 +18,27 @@ const getData = async () => {
   exhibitionArea.appendChild(pokeImage)
  }
  
+
+ const serchAreaBar = document.querySelector('[sc-search-area-bar]')
+ const searchButton = document.querySelector('[sc-search-button]')
+
+ searchButton.addEventListener('click', function(){
+   pokeIndentifier = serchAreaBar.value
+   pokeFetch()
+ })
+
+ function pokeFetch(){
+  let pokeURL = `https://pokeapi.co/api/v2/pokemon/${pokeIndentifier}`
+  const pokemon = fetch(pokeURL)
+  .then((resp) => resp.json())
+  
+  
+  const getData = async () => {
+    pokemon.then((a) => {
+      pokemonName(a)
+      pokemonSprite(a)
+    })
+  }
+  
+   getData()
+ }
